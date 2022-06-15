d5e1349d410b27b6711692d7c0f0811bbf9509fb
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-06-15 17:44:25 +0000

    betty corrections

--- instruction3.c
+++ instruction3.c
@@ -6,20 +6,21 @@
  */
 void instruct_mod(stack_t **stack, unsigned int line)
 {
-    int n;
-    if (var.len_stack < 2)
-    {
-        fprintf(stderr, "L%u: can't mod, stack too short\n", line);
-        exit(EXIT_FAILURE);
-    }
-    n = (*stack)->n;
-    instruct_pop(stack, line);
-    if (n == 0)
-    {
-        fprintf(stderr, "L%u: division by zero\n", line);
-        exit(EXIT_FAILURE);
-    }
-    (*stack)->n %= n;
+	int n;
+
+	if (var.len_stack < 2)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+	n = (*stack)->n;
+	instruct_pop(stack, line);
+	if (n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->n %= n;
 }
 /**
  *instruct_pchar - prints the char at the top of the stack
@@ -28,19 +29,20 @@ void instruct_mod(stack_t **stack, unsigned int line)
  */
 void instruct_pchar(stack_t **stack, unsigned int line)
 {
-    int ch;
-    if (var.len_stack < 1)
-    {
-        fprintf(stderr, "L%u: can't pchar, stack empty\n", line);
-        exit(EXIT_FAILURE);
-    }
-    ch = (*stack)->n;
-    if (!isascii(ch))
-    {
-        fprintf(stderr, "L%u: can't pchar, value out of range\n", line);
-        exit(EXIT_FAILURE);
-    }
-    printf("%c\n", ch);
+	int ch;
+
+	if (var.len_stack < 1)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line);
+		exit(EXIT_FAILURE);
+	}
+	ch = (*stack)->n;
+	if (!isascii(ch))
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", ch);
 }
 /**
  *instruct_pstr - prints the string starting at the top of the stack
@@ -49,20 +51,21 @@ void instruct_pchar(stack_t **stack, unsigned int line)
  */
 void instruct_pstr(stack_t **stack, unsigned int line __attribute__ ((unused)))
 {
-    stack_t *tmp;
-    int ch;
-    tmp = *stack;
-    while (tmp != NULL)
-    {
-        ch = tmp->n;
-        if (!isascii(ch) || ch == 0)
-        break;
-        putchar(ch);
-        tmp = tmp->next;
-        if (tmp == *stack)
-        break;
-    }
-    putchar('\n');
+	stack_t *tmp;
+	int ch;
+
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		ch = tmp->n;
+		if (!isascii(ch) || ch == 0)
+		break;
+		putchar(ch);
+		tmp = tmp->next;
+		if (tmp == *stack)
+		break;
+	}
+	putchar('\n');
 }
 /**
  *instruct_rotl - rotl rotates the stack to the top
@@ -71,8 +74,8 @@ void instruct_pstr(stack_t **stack, unsigned int line __attribute__ ((unused)))
  */
 void instruct_rotl(stack_t **stack, unsigned int line __attribute__ ((unused)))
 {
-    if (*stack)
-    *stack = (*stack)->next;
+	if (*stack)
+	*stack = (*stack)->next;
 }
 /**
  *instruct_rotr - rotr rotates the stack to the bottom
@@ -81,6 +84,6 @@ void instruct_rotl(stack_t **stack, unsigned int line __attribute__ ((unused)))
  */
 void instruct_rotr(stack_t **stack, unsigned int line __attribute__ ((unused)))
 {
-    if (*stack)
-    *stack = (*stack)->prev;
+	if (*stack)
+	*stack = (*stack)->prev;
 }
